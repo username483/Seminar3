875386cd23e6e227ec1d8d1da9b1757c4e112a1a
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-27 22:06:25 +0300

    Решена задача на вывод квадратов числа от 1 до N

---
+++ Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+class program
+{
+
+static void Main()
+{
+
+Console.Clear();
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine());
+
+if (N>0)
+{
+    int i=1;
+    while (N>=i)
+   {
+              Console.WriteLine(Math.Pow(i,2));
+              i ++;
+   }
+}
+if (N<0)
+{
+    int i=1;
+     while (N<=i)
+   {
+    Console.WriteLine(Math.Pow(i,2));
+              i --;
+   }
+}
+
+}
+}
