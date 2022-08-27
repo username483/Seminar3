956bf8193049e88321619b0b1c3d560852cd73c2
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-27 21:34:16 +0300

    Решена задача на поиск расстояния между координатами

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход координаты
+// двух точек  и находит расстояние между ними в 2Д пространстве
+// А(3,6) В (2,1) -> 5,09
+
+class program
+{
+
+static void Main()
+
+{
+    Console.Clear();
+Console.WriteLine("Введите значение Х для первой точки ");
+int X1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение Y для первой точки ");
+int Y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение Х для второй точки ");
+int X2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение Y для второй точки ");
+int Y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1)));
+}
+}
+
