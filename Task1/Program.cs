a935d1e0f5251051325a4e4ae2bdf843af25fe29
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-27 17:36:57 +0300

    Задача на нахождение четверти по координатам решена

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Напишите программу, которая принимает на вход координаты
+// точки (X,Y), причем X!=0, Y!=0 и выдает номер четверти плоскости,
+// в которой находится эта точка
+
+Console.Clear();
+Console.WriteLine("Введите X ");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X>0 && Y>0)
+{
+    Console.WriteLine("1 четверть");
+}
+else
+{
+     if(X<0 && Y>0)
+     {
+        Console.WriteLine("2 четверть");
+     }
+     else
+     {
+         if( X<0 && Y<0) 
+         {
+            Console.WriteLine("3 четверть");
+         }
+         else 
+         {
+            if (X>0 && Y<0)
+            {
+               Console.WriteLine("4 четверть"); 
+            }
+            else 
+            {
+                Console.WriteLine("одно или несколько введенных чисел =0, точка находится на оси координат, а не внутри четверти");
+            }
+         }
+     }
+
+}
